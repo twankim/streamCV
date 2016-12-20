489168c09f2e7bddb8471a4e5af62615b4cb8152
twankim <twankim@utexas.edu>
2016-12-19 23:25:06 -0600

    Record possible

--- test_stream_multi_v1.py
+++ test_stream_multi_v1.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import time
 import argparse
+from sys import platform
 
 def parse_args():
     parser = argparse.ArgumentParser(description=
@@ -15,6 +16,15 @@ def parse_args():
     parser.add_argument('-multi', dest='set_multi',
                         help='Use multiple camera sensors',
                         default = 1, type = int)
+    parser.add_argument('-verbose', dest='verbose',
+                        help='verbose is True if >= 1',
+                        default = 0, type = int)
+    parser.add_argument('-record', dest='record_on',
+                        help='Save recorded video if >0',
+                        default = 0, type = int)
+    parser.add_argument('-out', dest='out_name',
+                        help='Output file name',
+                        default = 'out', type = str)
     args = parser.parse_args()
     return args
 
@@ -28,17 +38,48 @@ def cam_init(cid,fps_input,seth):
     setw = int(frame.shape[1]*set_ratio)
 
     return {"cap":cap, "ret":ret, "set_ratio":set_ratio,\
-            "setw":setw, "detector": face_cascade}
+            "setw":setw, "detector": face_cascade,
+            "w_org":frame.shape[1],"h_org":frame.shape[0]}
+
+def vwriter_init(cid,fps_input,w,h):
+    if platform == "linux" or platform == "linux2":
+        # linux
+        fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    elif platform == "darwin":
+        # OS X
+        fourcc = cv2.cv.CV_FOURCC(*'mp4v')
+    elif platform == "win32":
+        # Windows...
+        fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    else:
+        fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    vout = cv2.VideoWriter('output_{}.mp4'.format(cid), fourcc, float(fps_input), (w,h))
+    return vout
 
 def main(args):
     fps_input = args.set_fps
     seth = args.set_height
     setm = args.set_multi
 
+    if args.verbose >= 1:
+        def vprint( obj ):
+            print obj
+    else:   
+        def vprint( obj ):
+            pass
+
+    if args.record_on > 0:
+        record_on = True
+    else:
+        record_on = False
+
     if setm < 1:
         setm = 1
 
     caps = [cam_init(cid,fps_input,seth) for cid in range(setm)]
+
+    if record_on:
+        vwriters = [vwriter_init(cid,fps_input,caps[cid]["w_org"],caps[cid]["h_org"]) for cid in range(setm)]
 
     while(True):
         # Capture frame-by-frame
@@ -58,7 +99,7 @@ def main(args):
                                                        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
                                                       ) for cid,cap in enumerate(caps)]
                                 
-        print "Detection... {:.3f}s ({} cameras)".format(time.time()-start,setm)
+        vprint("Detection... {:.3f}s ({} cameras)".format(time.time()-start,setm))
 
         frames = [ret_frame[1] for ret_frame in ret_frames]
 
@@ -73,6 +114,8 @@ def main(args):
 
         # Display the resulting frame
         for cid, frame in enumerate(frames):
+            if record_on:
+                vwriters[cid].write(frame)
             cv2.imshow('Cam {}'.format(cid),frame)
         
         if cv2.waitKey(1) & 0xff == ord('q'):
@@ -81,6 +124,8 @@ def main(args):
     # When everything done, release the capture
     for cap in caps:
         cap["cap"].release()
+        if record_on:
+            vwriters[cid].release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
